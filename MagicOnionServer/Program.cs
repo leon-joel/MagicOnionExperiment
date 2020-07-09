c681b1cc3d7ffef6982409cd6650ba32028ea8c7
leo <leopard10000@gmail.com>
2020-07-09 23:45:31 +0900

    gRPC / MagicOnion 入門 (13) - 送受信されているデータを可視化する
    https://blog.xin9le.net/entry/2017/09/22/014623

--- MagicOnionServer/Program.cs
+++ MagicOnionServer/Program.cs
@@ -11,11 +11,15 @@ namespace MagicOnionServer
 		{
 			// GrpcのLogging機構を使う
 			GrpcEnvironment.SetLogger(new ConsoleLogger());
-
 			GrpcEnvironment.Logger.Info("Server startup! =================================");
 
+			// MaginOnion側のログを gRPC のログに流し込む設定
+			// ※JSON形式でデータダンプする
+			var logger = new MagicOnionLogToGrpcLoggerWithDataDump();
+			var options = new MagicOnionOptions(true) { MagicOnionLogger = logger };
+
 			//--- ここで動的にアセンブリを解釈し、通信されてきたデータと API 本体とのマップを作る
-			var service = MagicOnionEngine.BuildServerServiceDefinition();
+			var service = MagicOnionEngine.BuildServerServiceDefinition(options);
 
 			//--- API を公開する IP / Port / 認証情報などを決定し、gRPC サーバーを起動
 			var port = new ServerPort("localhost", 12345, ServerCredentials.Insecure);
